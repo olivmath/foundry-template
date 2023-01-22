9bf0dd1a45d7ac7a519d19b8bdf01ab73b901025
Lucas Oliveira <olivmath@protonmail.com>
2023-01-22 09:34:51 -0300

    add: base setup test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,21 +3,27 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
+import {BaseSetup} from "./BaseSetup.sol";
 
-contract CounterTest is Test {
+contract CounterTest is Test, BaseSetup {
     Counter public counter;
 
-    function setUp() public {
+    function setUp() public virtual override{
+        BaseSetup.setUp();
+        vm.prank(owner);
         counter = new Counter();
+
         counter.setNumber(0);
     }
 
     function testIncrement() public {
+        vm.prank(bob);
         counter.increment();
         assertEq(counter.number(), 1);
     }
 
     function testSetNumber(uint256 x) public {
+        vm.prank(alise);
         counter.setNumber(x);
         assertEq(counter.number(), x);
     }
